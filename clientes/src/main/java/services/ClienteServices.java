bfec7806dce4f57fc7992d8f5af7258a24a1fa82
unknown <ericleite@hotmail.com>
2026-03-24 20:30:49 -0300

    Terceiro commit service criado

---
+++ clientes/src/main/java/services/ClienteServices.java
@@ -0,0 +1,31 @@
+package services;
+
+import entity.Cliente;
+import repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServices {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    public Cliente buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
