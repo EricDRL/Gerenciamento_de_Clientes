ee002294919004c6ddc188ba8a5614cd2b4f27e7
unknown <ericleite@hotmail.com>
2026-03-24 20:25:21 -0300

    Primeiro commit entity criado

---
+++ clientes/src/main/java/entity/Cliente.java
@@ -0,0 +1,53 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @Column(name = "email_cliente")
+    private String email;
+
+    @Column(name = "telefone_cliente")
+    private String telefone;
+
+    public Cliente() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
---
+++ clientes/src/test/java/er/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package er.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
